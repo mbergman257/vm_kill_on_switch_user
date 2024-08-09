c06b005cbeee979279c36b9a3d6d5385a18784b5
mbergman257 <mbergman257@gmail.com>
2024-08-08 20:31:49 -0400

    Update for Virtualbox instead of VMware

--- kill_vm_on_switch_user.ps1
+++ kill_vm_on_switch_user.ps1
@@ -22,15 +22,15 @@ function Write-Log {
     Add-Content -Path $logFilePath -Value $logMessage
 }
 
-# Get all processes with "vmware-vmx" in the name
-$vmwareProcesses = Get-Process | Where-Object { $_.ProcessName -like "*vmware-vmx*" }
+# Get all processes with "VirtualBoxVM" in the name, and sort list to kill largest Memory first
+$processes = Get-Process | Where-Object { $_.ProcessName -like "*VirtualBoxVM*" } | Sort-Object -Property  PrivateMemorySize
 
 # Get the current user that has just logged in
 $currentUser = (Get-WmiObject -Class Win32_ComputerSystem).UserName
 Write-Log "Current User: $currentUser"
 
 # Get the user that started each process and additional details
-foreach ($process in $vmwareProcesses) {
+foreach ($process in $processes) {
     try {
         $processDetails = Get-WmiObject Win32_Process -Filter "ProcessId = $($process.Id)"
         $processOwner = $processDetails.GetOwner()
@@ -40,17 +40,20 @@ foreach ($process in $vmwareProcesses) {
         $processUser = "$($processOwner.Domain)\$($processOwner.User)"
         
         Write-Log "Process: $($process.ProcessName) | PID: $($process.Id) | User: $processUser | CPU: $cpuUsage | Memory: $([math]::Round($memoryUsage, 2)) MB | Private Memory: $([math]::Round($privateMemorySize, 2)) MB"
-        
+
+        # Kill if the process was started by a different user
         if ($currentUser -ne $processUser) {
-            Write-Log "Different user: $($process.ProcessName) | PID: $($process.Id)"
+            Write-Log "`tDifferent user: $($process.ProcessName) | PID: $($process.Id)"
             try {
                 Stop-Process -Id $process.Id -Force
-                Write-Log "Process killed: $($process.ProcessName) | PID: $($process.Id)"
+                Write-Log "`tProcess killed: $($process.ProcessName) | PID: $($process.Id)"
             } catch {
-                Write-Log "Failed to kill process: $($process.ProcessName) | PID: $($process.Id)"
+                Write-Log "`tFailed to kill process: $($process.ProcessName) | PID: $($process.Id)"
             }
         }
     } catch {
         Write-Log "Could not retrieve details for process: $($process.ProcessName) | PID: $($process.Id)"
     }
 }
+# Add line for formatting
+Add-Content -Path $logFilePath -Value ""
